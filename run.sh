6cef6f8f4952aeebb2feef7757f9d24889febb10
Evan Hazlett <ejhazlett@gmail.com>
2013-08-27 19:44:14 -0400

    updated to remove kibana (will be separate repo)

--- run.sh
+++ run.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 CFG=${CFG:-}
+ES_HOST=${ES_HOST:-127.0.0.1}
+ES_PORT=${ES_PORT:-9300}
+EMBEDDED="false"
+
+if [ -z "$ES_HOST" ] ; then
+    EMBEDDED="true"
+fi
 
 if [ "$CFG" != "" ]; then
     wget $CFG -O /opt/logstash.conf --no-check-certificate
+else
+    cat << EOF > /opt/logstash.conf
+input {
+  syslog {
+    type => syslog
+    port => 514
+  }
+}
+output {
+  stdout { debug => true debug_format => "json"}
+  elasticsearch { embedded => $EMBEDDED host => "$ES_HOST" port => $ES_PORT }
+}
+EOF
 fi
 
-supervisord -c /etc/supervisor/supervisor.conf -n
+java -jar /opt/logstash.jar agent -f /opt/logstash.conf -- web --backend elasticsearch://$ES_HOST:$ES_PORT/
