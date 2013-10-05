5ce193e06fc7658e651bc74fb9f8cb12db077d5f
Evan Hazlett <ejhazlett@gmail.com>
2013-10-05 15:27:37 -0400

    fixed bug with config

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CFG=${CFG:-}
-ES_HOST=${ES_HOST:-127.0.0.1}
+ES_HOST=${ES_HOST:-}
 ES_PORT=${ES_PORT:-9300}
 EMBEDDED="false"
 
@@ -20,9 +20,18 @@ input {
 }
 output {
   stdout { debug => true debug_format => "json"}
+EOF
+    if [ "$EMBEDDED" = "true" ]; then
+        cat << EOF >> /opt/logstash.conf
+  elasticsearch { embedded => $EMBEDDED }
+}
+EOF
+    else
+        cat << EOF >> /opt/logstash.conf
   elasticsearch { embedded => $EMBEDDED host => "$ES_HOST" port => $ES_PORT }
 }
 EOF
+   fi
 fi
 
 java -jar /opt/logstash.jar agent -f /opt/logstash.conf -- web --backend elasticsearch://$ES_HOST:$ES_PORT/
